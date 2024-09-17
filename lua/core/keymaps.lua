2d06dd32bc6ac97ce0b1b40bc415bd1fae291fe8
vince <marc.bentoy@gmail.com>
2024-09-17 15:21:28 +0800

    update saving file/s hotkey

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,43 +1,44 @@
 local map = require("utils.keymaps")
 
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
 
 -- File
 map("n", "<leader>e", ":NvimTreeFindFile<CR>", { desc = "Find current file in Nvim Tree" })
-map("n", "<leader>ss", ":wa<CR>", { desc = "Save file" })
+map("n", "<leader>wf", ":w<CR>", { desc = "Save file" })
+map("n", "<leader>w.", ":wa<CR>", { desc = "Save all files" })
 
 -- fzf
-map('n', '<leader>ff', ':Files<CR>', { noremap = true, silent = true })
-map('n', '<leader>fb', ':Buffers<CR>', { noremap = true, silent = true })
+map("n", "<leader>ff", ":Files<CR>", { noremap = true, silent = true })
+map("n", "<leader>fb", ":Buffers<CR>", { noremap = true, silent = true })
 
 -- Buffers
 map("n", "<leader>vq", ":%bd<CR>:q<CR>", { desc = "Close neovim" })
 map("n", "<leader>qq", ":bd<CR>", { desc = "Close buffer and window" })
-map("n", "<leader>[", "<C-o>", { desc = "jump back" , noremap = true, silent = true })
-map("n", "<leader>]", "<C-i>", { desc = "jump forward" , noremap = true, silent = true })
+map("n", "<leader>[", "<C-o>", { desc = "jump back", noremap = true, silent = true })
+map("n", "<leader>]", "<C-i>", { desc = "jump forward", noremap = true, silent = true })
 
 -- Navigate to the next buffer
-map('n', '<Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+map("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
 
 -- Navigate to the previous buffer
-map('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
 
 -- Function to close current buffer and move to the next one
 function CloseCurrentBuffer()
-  local current_buffer = vim.api.nvim_get_current_buf()
-  vim.cmd('bd')
-  -- Move to the next buffer
-  vim.cmd('bnext')
+	local current_buffer = vim.api.nvim_get_current_buf()
+	vim.cmd("bd")
+	-- Move to the next buffer
+	vim.cmd("bnext")
 end
 
 -- Map the function to a key (e.g., <leader>c)
-vim.api.nvim_set_keymap('n', '<leader>x', ':lua CloseCurrentBuffer()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>x", ":lua CloseCurrentBuffer()<CR>", { noremap = true, silent = true })
 
 -- Close the current buffer without closing the window
-map('n', '<leader>C', ':bd!<CR>', { noremap = true, silent = true })
+map("n", "<leader>C", ":bd!<CR>", { noremap = true, silent = true })
 
 -- Close all other buffers except the current one
-map('n', '<leader>bo', ':BufferLineCloseOthers<CR>', { noremap = true, silent = true })
+map("n", "<leader>bo", ":BufferLineCloseOthers<CR>", { noremap = true, silent = true })
 
 -- tmux
 map("n", "<C-h>", ":TmuxNavigateLeft<CR>", { desc = "window left" })
@@ -92,27 +93,27 @@ map("n", "<leader>bda", ":%bd<CR>", { desc = "Close all buffers" })
 
 -- todo-comment plugin
 vim.keymap.set("n", "]t", function()
-  require("todo-comments").jump_next()
+	require("todo-comments").jump_next()
 end, { desc = "Next todo comment" })
 
 vim.keymap.set("n", "[t", function()
-  require("todo-comments").jump_prev()
+	require("todo-comments").jump_prev()
 end, { desc = "Previous todo comment" })
 
-vim.keymap.set('n', '<leader>/', ':CommentToggle<CR>', { desc = "Toggle Comment" })
-vim.keymap.set('v', '<leader>/', ":CommentToggle<CR>", { desc = "Toggle Comment" })
+vim.keymap.set("n", "<leader>/", ":CommentToggle<CR>", { desc = "Toggle Comment" })
+vim.keymap.set("v", "<leader>/", ":CommentToggle<CR>", { desc = "Toggle Comment" })
 
 map("n", "<leader>m", ":Format<CR>", { desc = "Format file" })
 map("v", "<leader>m", ":Format<CR>", { desc = "Format file" })
 
 vim.api.nvim_create_user_command("Format", function(args)
-        local range = nil
-        if args.count ~= -1 then
-                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-                range = {
-                        start = { args.line1, 0 },
-                        ["end"] = { args.line2, end_line:len() },
-                }
-        end
-        require("conform").format({ async = true, lsp_fallback = true, range = range })
+	local range = nil
+	if args.count ~= -1 then
+		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+		range = {
+			start = { args.line1, 0 },
+			["end"] = { args.line2, end_line:len() },
+		}
+	end
+	require("conform").format({ async = true, lsp_fallback = true, range = range })
 end, { range = true })
